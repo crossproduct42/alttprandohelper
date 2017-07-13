2bb981ed9d634cd92082cb0b502f524436fd47a8
Roger Norling <roger.norling.se@gmail.com>
2017-07-13 18:23:36 +0200

    Wrap scripts in function and use strict

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
-//launches tracker.html in a popup window. MUST ME CALLED FROM "index.html" (or similarly structured page)!!
-function launchTracker(){
-    mapEnabled = document.getElementById("option_mapEnabled").value;
-    mapEnabled = mapEnabled == "true";
-    mode = document.getElementById("option_mode").value;
+(function(window) {
+    'use strict';
 
-    if (mapEnabled){
-        windowSize = "width=1344, height=448"
-    } else {
-        windowSize = "width=448, height=448"
-    }
+    //launches tracker.html in a popup window. MUST ME CALLED FROM "index.html" (or similarly structured page)!!
+    window.launchTracker = function() {
+        var mapEnabled = document.getElementById("option_mapEnabled").value;
+        var mode = document.getElementById("option_mode").value;
+        var windowSize;
+        mapEnabled = mapEnabled == "true";
 
-    myWindow = window.open("tracker.html?map="+mapEnabled+"&mode="+mode, "", windowSize+", menubar=0 scrollbars=0, titlebar=0, resizable=0, toolbar=0");
-    setTimeout("window.close()", 5000);
+        if (mapEnabled){
+            windowSize = "width=1344, height=448"
+        } else {
+            windowSize = "width=448, height=448"
+        }
 
-}
+        window.open("tracker.html?map="+mapEnabled+"&mode="+mode, "", windowSize+", menubar=0 scrollbars=0, titlebar=0, resizable=0, toolbar=0");
+        setTimeout("window.close()", 5000);
+    };
 
-//a helper function that lets you access string variables from the URL.
-function getQueryVariable(variable){
-   var query = window.location.search.substring(1);
-   var vars = query.split("&");
-   for (var i=0;i<vars.length;i++) {
-       var pair = vars[i].split("=");
-       if(pair[0] == variable){return pair[1];}
-   }
-   return(null);
-}
+    //a helper function that lets you access string variables from the URL.
+    window.getQueryVariable = function(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+        }
+        return(null);
+    };
+}(window));
