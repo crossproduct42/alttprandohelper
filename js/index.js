2ff1d92299719332a47e238c1bdb5bf0f0bb58b8
Roger Norling <roger.norling.se@gmail.com>
2017-09-30 05:09:56 +0200

    Add NodeList#forEach polyfill for IE

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 (function(window) {
     'use strict';
 
+    if (window.NodeList && !NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = function(callback, _this) {
+            _this = _this || window;
+            for (var i = 0; i < this.length; i++) {
+                callback.call(_this, this[i], i, this);
+            }
+        };
+    }
+
     function launch_tracker() {
         var mode = this.getAttribute('data-mode'),
             map = this.getAttribute('data-map') === 'yes',
@@ -13,7 +22,7 @@
             'width={width},height=448,titlebar=0,menubar=0,toolbar=0,scrollbars=0,resizable=0'
                 .replace('{width}', width));
         setTimeout('window.close()', 5000);
-    };
+    }
 
     window.start = function() {
         document.querySelectorAll('.launch').forEach(
