a7ba9a4105d7659f1a07ca944b6b48beb84e7e26
Roger Norling <roger.norling.se@gmail.com>
2017-11-25 17:32:28 +0100

    Add announcer utility
    This utility implements the observer/observed pattern through dependency injection. It will facilitate communication between reactjs components and the previous implementation.

--- js/util.js
+++ js/util.js
@@ -1,6 +1,29 @@
 (function(window) {
     'use strict';
 
+    var slice = Array.prototype.slice;
+
+    window.announcer = function() {
+        var listeners = [],
+            methods = {};
+
+        function announce() {
+            var args = slice.call(arguments);
+            listeners.forEach(function(f) { f.apply(null, args); });
+        };
+
+        methods.on = function(f) {
+            listeners.push(f);
+        };
+
+        methods.off = function(f) {
+            var index = listeners.findIndex(function(x) { return x === f; });
+            if (index >= 0) listeners.splice(index, 1);
+        };
+
+        return Object.assign(announce, methods);
+    };
+
     window.counter = function(value, delta, max, min) {
         min = min || 0;
         value += delta;
